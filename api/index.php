d6a9b7e8b68db91b8779995df1ce377a3de85037
li1023qwq <130041731+li1023qwq@users.noreply.github.com>
2023-10-16 17:52:35 +0800

    Update index.php

--- api/index.php
+++ api/index.php
@@ -13,6 +13,15 @@
 		<input type="submit" value="登录">
 	</form>
 	<br>
+	<h1>注册</h1>
+	<form action="register.php" method="post">
+		<label for="username">用户名：</label>
+		<input type="text" id="username" name="username"><br><br>
+		<label for="password">密码：</label>
+		<input type="password" id="password" name="password"><br><br>
+		<input type="submit" value="登录">
+	</form>
+	<br>
 	<a href="reset_password.php">忘记密码？</a>
 </body>
 </html>
