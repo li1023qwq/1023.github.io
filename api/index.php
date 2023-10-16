8a43718b51b8063130a0657f25a397d9e74ddb1f
li1023qwq <130041731+li1023qwq@users.noreply.github.com>
2023-10-16 17:47:40 +0800

    Update index.php

--- api/index.php
+++ api/index.php
@@ -1,21 +1,18 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>登录注册示例</title>
+	<title>登录</title>
 </head>
 <body>
-    <h1>登录</h1>
-    <form action="login.php" method="post">
-        <input type="text" name="username" placeholder="用户名" required><br>
-        <input type="password" name="password" placeholder="密码" required><br>
-        <input type="submit" value="登录">
-    </form>
-
-    <h1>注册</h1>
-    <form action="register.php" method="post">
-        <input type="text" name="username" placeholder="用户名" required><br>
-        <input type="password" name="password" placeholder="密码" required><br>
-        <input type="submit" value="注册">
-    </form>
+	<h1>登录</h1>
+	<form action="login.php" method="post">
+		<label for="username">用户名：</label>
+		<input type="text" id="username" name="username"><br><br>
+		<label for="password">密码：</label>
+		<input type="password" id="password" name="password"><br><br>
+		<input type="submit" value="登录">
+	</form>
+	<br>
+	<a href="reset_password.php">忘记密码？</a>
 </body>
 </html>
